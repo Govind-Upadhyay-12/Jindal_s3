b4e3b56e63a651bdbd675422cad42ff5a40cc72a
Govind <you@example.com>
2024-07-24 13:30:55 +0530

    updating_code

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const AWS = require("aws-sdk");
-
 const app = express();
 const port = 8080;
-
 const s3 = new AWS.S3({
   accessKeyId: process.env.ACCESS_KEY,
   secretAccessKey: process.env.ACCESS_SECRET,
   region: process.env.REGION,
 });
-
 const listObjects = async (bucketName) => {
   try {
     const params = {
@@ -24,17 +21,14 @@ const listObjects = async (bucketName) => {
     throw error;
   }
 };
-
-const getPresignedUrl = (bucketName, key, expires = 60) => {
+const getPresignedUrl = (bucketName, key) => {
   const params = {
     Bucket: bucketName,
     Key: key,
-    Expires: expires, 
   };
 
   return s3.getSignedUrl("getObject", params);
 };
-
 const generateLinksForObjects = (bucketName, objects) => {
   return objects.map((obj) => ({
     key: obj.Key,
@@ -42,15 +36,13 @@ const generateLinksForObjects = (bucketName, objects) => {
     lastModified: obj.LastModified,
   }));
 };
-
-
 app.get("/s3-data", async (req, res) => {
   try {
     const bucketName = process.env.BUCKET;
     const objects = await listObjects(bucketName);
     const links = generateLinksForObjects(bucketName, objects);
 
-    res.json(links);
+   return res.status(200).json({statusCode:200,links:links});
   } catch (error) {
     console.error("Error accessing S3 data:", error);
     res.status(500).json({ error: "Internal Server Error" });
